4c12c07d5e47b432324c08e7ce8c92325b6710d3
Aaimry <wheal28@gmail.com>
2022-01-08 11:34:30 +0600

    Модель GuestBook зарегистрирована в админ панели

--- source/guest_book/admin.py
+++ source/guest_book/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from guest_book.models import GuestBook
+
+
+class GuestBookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'text']
+    list_filter = ['name', 'status']
+    search_fields = ['name', 'status']
+    fields = ['name', 'email', 'text', 'create_date', 'update_date', 'status']
+
+
+admin.site.register(GuestBook, GuestBookAdmin)
